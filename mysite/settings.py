05a4b70f7dff2339efc1ef4f8109bec78aaecff9
BJakeLando <bjlando@gmail.com>
2023-10-09 17:28:57 -0700

    production2

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 DEBUG = False
 
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-3640.up.railway.app']
 
 # Application definition
 
