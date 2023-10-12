9564bdc299ab06c54975dee191181d04a441f359
BJakeLando <bjlando@gmail.com>
2023-10-11 18:08:42 -0700

    production 6.1

--- pages/urls.py
+++ pages/urls.py
@@ -7,7 +7,7 @@ from .views import (
 )
 
 urlpatterns =[
-    path("home/", HomeView.as_view(), name ='home'),
+    path("", HomeView.as_view(), name ='home'),
     path("about/", AboutView.as_view(), name ='about'),
     path("pricing/", PricingView.as_view(), name ='pricing'),
 ]
