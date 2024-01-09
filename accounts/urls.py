79b90da55a6297373b869bd8a029498757c3ea9d
BJakeLando <bjlando@gmail.com>
2024-01-08 19:14:50 -0800

    deleted events page

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', SignupView.as_view(), name='signup'),
-    path('events/', views.all_events, name='events'),
     path('add_venue', views.add_event, name='add-event'),
     path('<int:pk>/delete/', views.EventDeleteView.as_view(), name='delete_event'),
 ]
