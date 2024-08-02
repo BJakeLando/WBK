302a6fdfcf7ba1e5a988396c60089d3f047bd288
BJakeLando <110437449+BJakeLando@users.noreply.github.com>
2024-08-02 16:12:39 -0700

    Updatets to reviews and gallery

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from blog import views
-
-urlpatterns = [
-    path('', views.PostListView.as_view(), name= 'list'),
-    path('new/', views.PostCreateView.as_view(), name= 'new'),
-    path('<int:pk>/delete/', views.PostDeleteView.as_view(), name= 'delete'),
-]
