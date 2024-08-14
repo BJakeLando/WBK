3aa36e17ce90f10d6e3df24dee6d2dba23d153f1
BJakeLando <110437449+BJakeLando@users.noreply.github.com>
2024-08-13 19:40:16 -0700

    small update

--- reviews/views.py
+++ reviews/views.py
@@ -6,6 +6,12 @@ class PostListView(ListView):
     template_name= "reviews/list.html"
     model = Review
 
+    def list_reviews(self,name):
+        issue_list = Review.objects.all().order_by('created_on')
+        return issue_list
+          
+
+
 class PostDetailView(DetailView):
     template_name = "reviews/detail.html"
     model = Review
@@ -13,4 +19,4 @@ class PostDetailView(DetailView):
 class PostCreateView(CreateView):
     template_name = "reviews/new.html"
     model = Review
-    fields = ['name','title', 'body']
+    fields = ['name','title', 'body', 'source']
