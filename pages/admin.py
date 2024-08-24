9f95cffb0d0f14f29869059e27bd3d2170e1061f
BJakeLando <110437449+BJakeLando@users.noreply.github.com>
2024-08-24 12:56:41 -0700

    Added video to Homepage

--- pages/admin.py
+++ pages/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from embed_video.admin import AdminVideoMixin
+from .models import Video
+
+class AdminVideoMixin(AdminVideoMixin):
+    pass
+
+admin.site.register(Video)
