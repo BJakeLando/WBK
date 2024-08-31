796cfb91ed3154b27f0a99febf5b9b98adbe962c
BJakeLando <110437449+BJakeLando@users.noreply.github.com>
2024-08-31 13:40:17 -0700

    added FAQ

--- pages/admin.py
+++ pages/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from embed_video.admin import AdminVideoMixin
-from .models import Video
-
-class AdminVideoMixin(AdminVideoMixin):
-    pass
-
-admin.site.register(Video)
