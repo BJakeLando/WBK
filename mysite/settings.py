c440e2629930c345df0191c5cc5c2c8f2ecfd09c
BJakeLando <110437449+BJakeLando@users.noreply.github.com>
2024-09-02 13:46:26 -0700

    Debug = False

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ mimetypes.add_type("text/javascript", ".js", True)
 # SECRET_KEY = 'django-insecure-n-#+(!vpu0s#5p8fpxwbtstscrsf5uhbiew@aoir09b+unxe#)'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8_a+-$4w1f^lt3lx1c@d*p4x$ymm_rn7pwb87')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
