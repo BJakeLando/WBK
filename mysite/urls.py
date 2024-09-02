0c5a6c0b7792281ca25521059ba445aeab612e17
BJakeLando <110437449+BJakeLando@users.noreply.github.com>
2024-09-02 14:32:32 -0700

    debug = T

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,4 +27,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('reviews/', include('reviews.urls')),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
