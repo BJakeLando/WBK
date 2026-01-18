4d1a7c97b31e945397c12a6f0777d85e0a71d03b
BJakeLando <110437449+BJakeLando@users.noreply.github.com>
2026-01-17 19:09:04 -0700

    debugas

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'www.paintedbykarla.com']
 
