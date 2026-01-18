a1272405b1da87d045ed37faa70f597b327c8a38
BJakeLando <110437449+BJakeLando@users.noreply.github.com>
2026-01-17 18:29:35 -0700

    Added Video

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,30 +1,24 @@
 from django.contrib import admin
-from .models import LivePaintEvent
-from .models import MyClientUser
-from django.contrib.auth.models import Group,User
+from .models import LivePaintEvent, MyClientUser
+from django.contrib.auth.models import Group, User
+from .admin_calendar import CalendarAdminMixin
 
 admin.site.unregister(Group)
 admin.site.register(MyClientUser)
 
-
 class UserAdmin(admin.ModelAdmin):
     model = User
-    fields = [
-        "username"
-    ]
+    fields = ["username"]
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
-class UserAdmin(admin.ModelAdmin):
-    model = MyClientUser
-    # Just display username fields on admin page
-    fields = ["username"]
-    inlines = [MyClientUser]
-
 @admin.register(LivePaintEvent)
-class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('created_on', 'name','venue', 'event_date', 'email', 'budget', 'typeofclient',)
+class LivePaintEventAdmin(CalendarAdminMixin, admin.ModelAdmin):
+    list_display = ('created_on', 'name', 'venue', 'event_date', 'email', 'budget', 'typeofclient',)
     ordering = ('-created_on',)
     search_fields = ('name', 'venue', 'event_date',)
-    list_filter = ('name', 'venue', 'event_date','budget',)
+    list_filter = ('name', 'venue', 'event_date', 'budget',)
+    
+    # Add calendar link to admin
+    change_list_template = 'admin/livepaintevent_changelist.html'
