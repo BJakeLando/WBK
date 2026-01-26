27ea9d59a37d56b9b1b251b43f9e5ede252fc2d4
BJakeLando <110437449+BJakeLando@users.noreply.github.com>
2026-01-26 13:14:30 -0700

    Fix setup_admin.py imports

--- setup_admin.py
+++ setup_admin.py
@@ -1,3 +1,11 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
