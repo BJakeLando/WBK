6a5dca3f747fce006996da93b4627c33ea2b159e
JakeLando <bjlando@gmail.com>
2026-05-25 21:39:47 +0000

    Update reviews page, navbar, homepage styling

--- accounts/forms.py
+++ accounts/forms.py
@@ -33,7 +33,7 @@ class EventForm(ModelForm):
             'name':forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Bride & Groom or Partner Names'}), 
             'phone':forms.TextInput(attrs={'class': 'form-control','placeholder': 'Phone Number'}),
             'email':forms.EmailInput(attrs={'class': 'form-control','placeholder': 'Email'}),
-            'event_date':forms.TextInput(attrs={'class': 'form-control','placeholder': 'Event Date (MO/DD/YYYY)'}),
+            'event_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'venue_name':forms.TextInput(attrs={'class': 'form-control','placeholder': 'Venue Name'}),
             'venue':forms.TextInput(attrs={'class': 'form-control','placeholder': 'Venue Location (City, St)'}),
             'reference':forms.TextInput(attrs={'class': 'form-control','placeholder': 'How did you about us?'}),
@@ -43,9 +43,13 @@ class EventForm(ModelForm):
             'description':forms.TextInput(attrs={'class': 'form-control','placeholder': 'Brief Description of event. ( Wedding, Birthday, etc )'}),
             'source':forms.TextInput(attrs={'class': 'form-control','placeholder': 'How did you hear about me?'}),
             'instagram':forms.TextInput(attrs={'class': 'form-control','placeholder': 'What is your Instagram?'}),
-            'budget':forms.TextInput(attrs={'class': 'form-control','placeholder': 'Estimated budget for live painting? '}),
+            'budget': forms.Select(attrs={'class': 'form-control'}, choices=[
+                ('', 'What is your budget for live painting?'),
+                ('below_1500', 'Below $1,500'),
+                ('above_1500', '$1,500 and above'),
+            ]),
             'choice':forms.TextInput(attrs={'class': 'form-control','placeholder': 'Inquiring for Guests or Bride and Groom Painting?'}),
-            'typeofclient':forms.TextInput(attrs={'class': 'form-control','placeholder': 'Just Curious/Shopping Around... or I would love for you to be there! '}),
+            'typeofclient':forms.TextInput(attrs={'class': 'form-control','placeholder': 'I would love for you to be there or just curious?'}),
             'description':forms.TextInput(attrs={'class': 'form-control','placeholder': 'Brief Description of event. ( Wedding Dress Code, Birthday, Corporate Event, ETC)', 'rows': 3}),
 
 
